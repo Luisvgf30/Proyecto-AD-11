d435d8984e97883fe2d87cea2f139c0e4b160dda
ricardodr13 <ricardodr13@gmail.com>
2024-01-29 19:55:43 +0100

    Conexion a base de datos

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongodb: {
-    URI: 'mongodb+srv://ricardodr13:<eQYoUORHFA3S7pt8>@tema11.kvjyb6h.mongodb.net/?retryWrites=true&w=majority'
+    URI: 'mongodb+srv://ricardodr13:eQYoUORHFA3S7pt8@tema11.kvjyb6h.mongodb.net/?retryWrites=true&w=majority'
   }
 };
