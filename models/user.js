464ef108bfe9c8042060da9d749d6c9988c1d6a6
Raulpruebasgh <Raulpruebasgh@gmail.com>
2024-02-05 19:47:21 +0100

    users4

--- models/user.js
+++ models/user.js
@@ -31,6 +31,17 @@ userSchema.methods.findEmail= async (email) => {
 
 };
 
+userSchema.methods.findId= async (id) => {
+  const User = mongoose.model("user", userSchema);
+  return  await User.findOne({'id': id})
+
+};
+
+userSchema.methods.findAll= async function () {
+  const User = mongoose.model("user", userSchema);
+  return await User.find();
+};
+
 userSchema.methods.findTipo= async (tipo) => {
   const User = mongoose.model("user", userSchema);
   return  await User.find({'tipo': tipo})
