274d60a3e6760b2a19a158d6847055be01c5f047
Luisvgf30 <luisvgf30@gmail.com>
2024-02-06 19:35:18 +0100

    diseño

--- public/javascripts/stylesjs.js
+++ public/javascripts/stylesjs.js
@@ -1,6 +1,5 @@
-$( '#multiple-select-disabled-field' ).select2( {
+$( '#single-select-field' ).select2( {
     theme: "bootstrap-5",
     width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
     placeholder: $( this ).data( 'placeholder' ),
-    closeOnSelect: false,
 } );
