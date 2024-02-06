bb7e26e01f777698eee6811225da6a313cb53091
Luisvgf30 <luisvgf30@gmail.com>
2024-02-06 20:51:13 +0100

    diseño2

--- public/javascripts/stylesjs.js
+++ public/javascripts/stylesjs.js
@@ -1,5 +1,5 @@
-$( '#single-select-field' ).select2( {
-    theme: "bootstrap-5",
-    width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
-    placeholder: $( this ).data( 'placeholder' ),
-} );
+$(document).ready(function() {
+    $('#multiple-checkboxes').multiselect({
+      includeSelectAllOption: true,
+    });
+});
