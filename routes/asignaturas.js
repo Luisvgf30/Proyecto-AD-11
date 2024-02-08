3cbf57a573612dfffc745c78b6412f01ef7f5af6
Raulpruebasgh <Raulpruebasgh@gmail.com>
2024-02-08 21:00:30 +0100

    as3

--- routes/asignaturas.js
+++ routes/asignaturas.js
@@ -16,12 +16,7 @@ router.get('/asignaturas',isAuthenticated, async (req, res) => {
   });
 });
 
-router.post('/asignaturas/add', isAuthenticated,async (req, res, next) => {
-  const asignatura = new Asignatura(req.body);
-  asignatura.usuario=req.user._id;
-  await asignatura.insert();
-  res.redirect('/asignaturas');
-});
+
 
 router.get('/asignaturas/turn/:id',isAuthenticated, async (req, res, next) => {
   let { id } = req.params;
@@ -53,6 +48,75 @@ router.post('/asignaturas/edit/:id',isAuthenticated, async (req, res, next) => {
   res.redirect('/asignaturas');
 });
 
+// metodo delete asignatura ***************************************
+router.get('/asignaturas/delete/:id', isAuthenticated, async (req, res, next) => {
+  let Asignatura = mongoose.model('asignatura');
+  let Usuario = mongoose.model('user');
+  let { id } = req.params;
+  
+  // Encuentra la asignatura por id
+  let asignatura = await Asignatura.findById(id);
+  
+  // Comprueba si asignatura.profesores y asignatura.alumnos son arrays
+  if (Array.isArray(asignatura.profesores)) {
+    for (let profesorId of asignatura.profesores) {
+      let profesor = await Usuario.findById(profesorId);
+      await profesor.editAsignaturas(profesor.id, id);
+    }
+  }
+  
+  if (Array.isArray(asignatura.alumnos)) {
+    for (let alumnoId of asignatura.alumnos) {
+      let alumno = await Usuario.findById(alumnoId);
+      await alumno.editAsignaturas(alumno.id, id);
+    }
+  }
+  
+  // Elimina la asignatura
+  await Asignatura.deleteOne({ _id: id });
+  
+  res.redirect('/asignaturas');
+});
+
+// metodo add asignatura ***************************************
+router.post('/asignaturas/add', isAuthenticated,async (req, res, next) => {
+  let Usuario = mongoose.model('user');
+  const asignatura = new Asignatura(req.body);
+  asignatura.usuario=req.user._id;
+  await asignatura.insert();
+  res.redirect('/asignaturas');
+
+  // agregar la asignatura a profesor
+   // Encuentra la asignatura por id
+   let asignaturas = await Asignatura.findAll(); 
+   asignatura = asignaturas[asignaturas.length -1];
+   
+
+  // Comprueba si asignatura.profesores y asignatura.alumnos son arrays
+  if (Array.isArray(asignatura.profesores)) {
+    for (let profesorId of asignatura.profesores) {
+      let profesor = await Usuario.findById(profesorId);
+      await profesor.editAsignaturas(profesor.id, asignatura.id);
+    }
+  }
+
+
+  // agregar la asignatura a los alumnos
+
+  if (Array.isArray(asignatura.alumnos)) {
+    for (let alumnoId of asignatura.alumnos) {
+      let alumno = await Usuario.findById(alumnoId);
+      await alumno.editAsignaturas(alumno.id, asignatura.id);
+    }
+  }
+
+    
+
+
+});
+
+
+// metodo update asignatura *****************************************************
 router.get('/asignaturas/delete/:id', isAuthenticated, async (req, res, next) => {
   let Asignatura = mongoose.model('asignatura');
   let Usuario = mongoose.model('user');
