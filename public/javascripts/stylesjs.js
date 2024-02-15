cfe9c578de21b504c9a34901653162e843f4ea50
Luisvgf30 <luisvgf30@gmail.com>
2024-02-15 19:12:57 +0100

    error del select

--- public/javascripts/stylesjs.js
+++ public/javascripts/stylesjs.js
@@ -1,28 +1,14 @@
-$( '#single-select-field' ).select2( {
-    theme: "bootstrap-5",
-    width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
-    placeholder: $( this ).data( 'placeholder' ),
-} );
-
-$('#basic').multiselect({
-    templates: {
-        li: '<li><a href="javascript:void(0);"><label class="pl-2"></label></a></li>'
-    }
-});
-
-$(document).ready(function() {
-    $('#multiple-checkboxes').multiselect({
-      includeSelectAllOption: true,
-    });
-});
-
 function changeFunc() {
-    alert();
-        var selectBox = document.getElementById("selectBox");
+    var selectBox = document.getElementById("selectBox");
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+
     if(selectedValue == "Administrador"){
         document.getElementById("asig").style.display="none";
+
     }else{
         document.getElementById("asig").style.display="block";
+
     }
    }
+
+   
