2f4088130b3514e36d670050b252eec54d00b60a
Luisvgf30 <luisvgf30@gmail.com>
2024-03-09 13:02:31 +0100

    plan de estudios

--- public/javascripts/stylesjs.js
+++ public/javascripts/stylesjs.js
@@ -1,14 +1,39 @@
+document.getElementById("asigGrado").style.display="none";
+document.getElementById("asigMaster").style.display="none"
+document.getElementById("asigDoctorado").style.display="none"
+
 function changeFunc() {
     var selectBox = document.getElementById("selectBox");
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
 
     if(selectedValue == "Administrador"){
-        document.getElementById("asig").style.display="none";
+        document.getElementById("asigGrado").style.display="none";
+        document.getElementById("asigMaster").style.display="none";
+        document.getElementById("asigDoctorado").style.display="none";
+        document.getElementById("divplanEstudio").style.display="none";
 
     }else{
-        document.getElementById("asig").style.display="block";
-
+        document.getElementById("divplanEstudio").style.display="block";
     }
-   }
+}
 
-   
+function changeFunc2() {
+    var selectBox = document.getElementById("planEstudio");
+    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+
+    if(selectedValue == "Grado"){
+        document.getElementById("asigGrado").style.display="block";
+        document.getElementById("asigMaster").style.display="none"
+        document.getElementById("asigDoctorado").style.display="none"
+
+    }else if(selectedValue == "Master"){
+        document.getElementById("asigGrado").style.display="none"
+        document.getElementById("asigMaster").style.display="block";
+        document.getElementById("asigDoctorado").style.display="none"
+
+    }else if(selectedValue == "Doctorado"){
+        document.getElementById("asigGrado").style.display="none"
+        document.getElementById("asigMaster").style.display="none"
+        document.getElementById("asigDoctorado").style.display="block";
+    }
+}
